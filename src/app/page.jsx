ed0fab3b80d9515f0da46d8cca64eabc491d5c9a
MoFares7 <faresdabbas1@gmail.com>
2023-05-30 17:52:05 +0300

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import LoginPage from './pages/LoginPage';
+
+
+export default function Home() {
+    return (
+        <div className='Main-div'>
+        <LoginPage />
+        </div>
+    );
+}
---
+++ src/app/pages/LoginPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function LoginPage() {
+        return (
+                <div class="Main-Login w-full  max-h-full">
+                        <div class=" w-full  flex md:max-h-screen">
+                                <div class="First-section max-h-full w-full bg-purple-600 disp " >
+                                        <h1>fares</h1>
+                                        <h1>fares</h1>
+                                </div>
+                                <div class="Second-section max-h-full w-full bg-whait bg-gray-50" >
+                                        <h1>fares</h1>
+                                        <h1>fares</h1>
+                                </div>
+                        </div>
+                </div>
+        );
+}
